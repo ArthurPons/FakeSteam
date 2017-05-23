591f024b95f0567f7e5e30a4e6ef8f7be5b34c72
ArthurPons <pons.arthur@gmail.com>
2017-05-23 11:09:49 +0200

    Cleanup

--- src/dao/HistoricDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-import bean.Historic;
-
-public interface HistoricDao {
-	void create( Historic historic ) throws DaoException;
-	Historic find( int id ) throws DaoException;
-}
--- src/dao/RatingDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import bean.Rating;
-
-public interface RatingDao {
-
-	void create( Rating rating ) throws DaoException;
-	Rating find( int id ) throws DaoException;
-}
